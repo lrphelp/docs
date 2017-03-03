6dae2de42fca6c5aa8a0e59d6ee7acd8dde76b10
lrporder <m.koelewijn@protestantsekerk.nl>
2017-03-03 16:37:05 +0100

    Fix navbar

--- js/main.js
+++ js/main.js
@@ -1,17 +1,19 @@
 
 $(document).ready(function () {
+	// generate toc
 	$('#toc').toc({ title: "Ga naar..." });
-	// START offcanvas.js
+	
+	// START offcanvas.js (this does not work)
 	$('[data-toggle="offcanvas"]').click(function () {  
 		$('.row-offcanvas').toggleClass('active')
 	}); //END offcanvas.js
 	
 	// activate current navbar item
-	var currentPage = window.location.href.split('/')[3];
-	console.log("Current page is " + currentPage);
-	if(currentPage !== ""){
-		console.log("Current page is not empty");
-		$('#navbar .active').removeClass('active');
-		$('#' + currentPage).addClass('active');
+	var currentPage = window.location.href.split('/')[3]; // check where we are
+	$('#navbar .active').removeClass('active');		// deactivate previously activated item
+	if(currentPage !== ""){							// make sure were not at home
+		$('#' + currentPage).addClass('active');	// activate currently activated item
+	} else {
+		$('#navHome').addClass('active');			// or activate Home
 	};
 });
