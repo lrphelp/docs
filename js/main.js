3e2007e18ffaa07e22e9226f58635a7a1514241d
lrporder <m.koelewijn@protestantsekerk.nl>
2017-03-03 16:20:03 +0100

    again

--- js/main.js
+++ js/main.js
@@ -8,7 +8,9 @@ $(document).ready(function () {
 	
 	// activate current navbar item
 	var currentPage = window.location.href.split('/')[3];
+	console.log("Current page is " + currentPage);
 	if(currentPage !== ""){
+		console.log("Current page is not empty");
 		$('#navbar .active').removeClass('active');
 		$('#' + currentPage).addClass('active');
 	};
